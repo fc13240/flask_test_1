c5f5aed8135960f659d97148835f224bf57f248c
HamYu <1669387345@qq.com>
2017-12-20 11:37:04 +0800

    f

--- static/js/trustsql.js
+++ static/js/trustsql.js
@@ -79,8 +79,8 @@ $(function() {
 			'pInfoKey': pInfoKey,
 			'nInfoVersion': nInfoVersion,
 			'nState': nState,
-			'pContent': JSON.stringify(pContent),
-			'pNotes': JSON.stringify(pNotes),
+			'pContent': pContent,
+			'pNotes': pNotes,
 			'pCommitTime': pCommitTime
 		};
 
